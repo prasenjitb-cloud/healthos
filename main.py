706ec85eee48d4497656bd745b267122422c5e44
Sujith Vaishnav <sujithvaishnavmalla@gmail.com>
2026-01-20 00:59:04 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from core.model_loader import load_tinyllama
-from core.chatbot import generate_response
-from memory.vector_memory import init_memory
+import core.chatbot
+import vector_db.vector_memory
+from llama_cpp import Llama
+
+def load_tinyllama():
+  
+    return Llama(
+        model_path="models/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",
+        n_ctx=2048,
+        temperature=0.3,
+        top_p=0.9,
+        n_threads=8,
+        verbose=False
+    )
 
 def main():
     init_memory()
