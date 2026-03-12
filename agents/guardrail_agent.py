719ec8e6cc79a68972753dcda4652dadac1d5d73
Sujith Vaishnav <sujithvaishnavmalla@gmail.com>
2026-03-12 15:48:48 +0530

    Update guardrail_agent.py

--- agents/guardrail_agent.py
+++ agents/guardrail_agent.py
@@ -3,7 +3,7 @@ import transformers
 
 def load_slm():
     return llama_cpp.Llama(
-        model_path="models/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",
+        model_path="models/BioMedLM-7B.Q4_K_M.gguf",
         n_ctx=2048,
         n_threads=4,
         verbose=False
