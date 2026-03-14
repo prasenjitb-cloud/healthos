4d6664229b1bee406dae1b5f0648546f5a6c7478
Sujith Vaishnav <sujithvaishnavmalla@gmail.com>
2026-03-14 17:08:37 +0530

    Create config.py

---
+++ agents/config.py
@@ -0,0 +1,13 @@
+BioMedLM_CONFIG = {
+    "model_path": "models/BioMedLM-7B.Q4_K_M.gguf",
+    "n_ctx": 2048,
+    "n_threads": 4,
+    "verbose": False
+}
+
+TinyLlama_CONFIG = {
+        "model_path"="models/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf",
+        "n_ctx"=2048,
+        "n_threads"=4,
+        "verbose"=False
+}
